0f043763271579d032483e13990a516a3452025a
Emilee <emiwemi495@gmail.com>
2023-01-31 20:31:31 +0000

    user input working

--- main.c
+++ main.c
@@ -3,30 +3,26 @@
 #include "heap.h"
 
 // constants
-#define MAX_EMPLOYEES 5 /** maximum allowed length of Employees array */
+#define MAX_EMPLOYEES 2 /** maximum allowed length of Employees array */
 
 // The driver function should create an array of MAX_EMPLOYEES elements, and fill it with values from the user.
 int main(int argc, char *argv[])
 {
 	Employee E[MAX_EMPLOYEES];
+	int size_e = sizeof(E) / sizeof(E[0]);
 	char name[MAX_NAME_LEN];
 	int salary;
 
-	Employee test_worker = { "Goose Duckduck\0", 2000};
-    for (int i = 0; i < MAX_EMPLOYEES; ++i) {
-        //
-    }
-
-
 	for (int i = 0; i < MAX_EMPLOYEES; i++)
 	{
-		printf("Enter the name of an Employee: ");
-		scanf("%s", &name);
-		E[i].name = name;
+		printf("Employee's name: ");
+		scanf("%s", E[i].name);
 
-		printf("Enter Employee %s's salary: ", &name);
-		scanf("%d", &salary);
-		E[i].salary = salary;
+		printf("%s's salary: ", E[i].name);
+		scanf("%d", &E[i].salary);
+
+		printf("\n");
 	}
+	heapSort(E,size_e);
 	return 0;
 }
